c193818849060b895e4ba3e4463dd587ea8b61ff
genfengl <genfeng.liu@gmail.com>
2022-11-19 17:14:10 +0800

    changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -34,32 +34,36 @@ const Login = () => {
                 </div>
                 {/* Main login section */}
                 <div className="flex flex-col gap-6 justify-center items-center rounded-lg
-                mx-3 p-6 bg-lightWhite">
+                mx-3 p-6 bg-lightWhite filter shadow-md shadow-gray-500">
                     <div className="text-3xl font-black text-dcBlue">
                         Welcome Back!
+                        <div className="text-base font-normal text-center mt-1 text-dcBlue">
+                            Login
+                        </div>
                     </div>
+
                     <form className="flex flex-col gap-6 w-60">
                         <label htmlFor="username" className="flex flex-col">
-                            <div className="text-dcBlue">
+                            {/* <div className="text-dcBlue">
                                 Username
                                 <span className="text-red-500" > *</span>
-                            </div>
-                            <input type="text" name="username" value={loginFields.username} onChange={handleLoginChange} placeholder=" username"
-                                className="border-2 rounded-lg p-2 mt-1 bg-lightWhite text-materialBlack" />
+                            </div> */}
+                            <input type="text" name="username" value={loginFields.username} onChange={handleLoginChange} placeholder=" username *"
+                                className="border-b p-2 bg-lightWhite text-materialBlack" />
                         </label>
                         <label htmlFor="password" className="flex flex-col">
-                            <div className="text-dcBlue">
+                            {/* <div className="text-dcBlue">
                                 Password
                                 <span className="text-red-500"> *</span>
-                            </div>
-                            <input type="text" name="password" value={loginFields.password} onChange={handleLoginChange} placeholder=" password"
-                                className="border-2 rounded-lg p-2 mt-1 bg-lightWhite text-materialBlack" />
+                            </div> */}
+                            <input type="text" name="password" value={loginFields.password} onChange={handleLoginChange} placeholder=" password *"
+                                className="border-b p-2 bg-lightWhite text-materialBlack" />
                         </label>
                         {/* Create account button */}
-                        <label className="flex flex-col">
+                        <label className="flex flex-col mt-3">
                             <input type="submit" value="LOGIN" className="flex p-2 rounded-md 
-                            justify-center transition-all bg-dcBlue text-[#fafafa] hover:bg-white hover:text-black" />
-                            <div className="flex gap-1 mt-2 justify-center text-gray-500">
+                            justify-center transition-all bg-dcBlue text-lightWhite hover:bg-white hover:text-black" />
+                            <div className="flex gap-1 mt-3 justify-center text-gray-500">
                                 New to us? <a href='' className="underline text-dcBlue"> Register</a>
                             </div>
 
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -29,35 +29,38 @@ const Register = () => {
                 </div>
                 {/* Main login section */}
                 <div className="flex flex-col gap-6 justify-center items-center rounded-lg
-                mx-3 p-6 bg-lightWhite filter drop-shadow-2xl">
+                mx-3 p-6 bg-lightWhite filter shadow-2xl shadow-gray-500">
                     <div className="text-3xl font-black text-dcBlue">
                         Create an account
+                        <div className="text-base font-normal text-center mt-1 text-dcBlue">
+                            Register
+                        </div>
                     </div>
                     <form className="flex flex-col gap-6 w-60">
                         <label htmlFor="username" className="flex flex-col">
-                            <div className="text-dcBlue">
+                            {/* <div className="text-dcBlue">
                                 Username
                                 <span className="text-red-500" > *</span>
-                            </div>
-                            <input type="text" name="username" value={registerFields.username} onChange={handleRegisterChange} placeholder=" username"
-                                className="border-2 rounded-lg p-2 mt-1 bg-lightWhite text-materialBlack" />
+                            </div> */}
+                            <input type="text" name="username" value={registerFields.username} onChange={handleRegisterChange} placeholder=" username *"
+                                className="border-b p-2 mt-1 bg-lightWhite text-materialBlack" />
                         </label>
                         <label htmlFor="password" className="flex flex-col">
-                            <div className="text-dcBlue">
+                            {/* <div className="text-dcBlue">
                                 Password
                                 <span className="text-red-500"> *</span>
-                            </div>
-                            <input type="text" name="password" value={registerFields.password} onChange={handleRegisterChange} placeholder=" password"
-                                className="border-2 rounded-lg p-2 mt-1 bg-lightWhite text-materialBlack"/>
-                            <input type="text" name="confirmPassword" value={registerFields.confirmPassword} onChange={handleRegisterChange} placeholder=" confirm password"
-                                className="border-2 rounded-lg p-2 mt-1 bg-lightWhite text-materialBlack" />
+                            </div> */}
+                            <input type="text" name="password" value={registerFields.password} onChange={handleRegisterChange} placeholder=" password *"
+                                className="border-b p-2 mt-1 bg-lightWhite text-materialBlack"/>
+                            <input type="text" name="confirmPassword" value={registerFields.confirmPassword} onChange={handleRegisterChange} placeholder=" confirm password *"
+                                className="border-b p-2 mt-1 bg-lightWhite text-materialBlack" />
                         </label>
                         {/* Create account button */}
                         <label className="flex flex-col">
-                            <input type="submit" value="CREATE ACCOUNT" className="flex rounded-lg p-2 
+                            <input type="submit" value="CREATE ACCOUNT" className="flex rounded-md p-2 
                             justify-center transition-all bg-dcBlue text-lightWhite hover:bg-white hover:text-black" />
 
-                            <div className="flex gap-1 justify-center mt-1">
+                            <div className="flex gap-1 justify-center mt-3">
                                 <a href='' className="underline text-dcBlue">Already have an account?</a>
                             </div>
                         </label>
