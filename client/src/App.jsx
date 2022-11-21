f678d33d82db0b858fead15d5c56c4f6915feb0d
Chin Ying Kong <chinYingAdora>
2022-11-22 09:47:58 +1100

    Added UserProfile under unprotected route so I can access and test it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import { useContext } from "react"
 import Chat from "./components/Chat"
 import EditProfile from "./components/EditProfile"
+import UserProfile from "./components/UserProfile"
 import { AuthContext } from "./AuthContext"
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
               <Route path='register' element={<Register />} />
               {/* login page */}
               <Route path='login' element={<Login />} />            
+              <Route path='userprofile' element={<UserProfile />} />          
             </Route>
           </Routes>
         </BrowserRouter>
