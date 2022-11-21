d71b8e894fe23f66e05281512e56234169b176ab
Chin Ying Kong <chinYingAdora>
2022-11-21 11:53:16 +1100

    Small changes to test EditProfile

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -3,11 +3,17 @@
 // import { query, collection, orderBy } from 'firebase/firestore';
 // import { db } from '../Firebase-config'
 // import Messages from "./Messages";
+import { Link } from 'react-router-dom'
 import Search from "./Search";
 
 const Chat = () => {
     
-    return <Search />
+    return (
+    <>
+        <Search />
+        <Link to="/chatapp/editProfile">Edit Profile</Link>
+    </>
+        )
 }
 
 export default Chat
