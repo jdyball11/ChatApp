3a8d97c8153ce00f20ae536dd8f268c59b468c97
Chin Ying Kong <chinYingAdora>
2022-11-22 14:06:54 +1100

    Deleted my tested link to EditProfile

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,6 @@
 // import Messages from "./Messages";
 import Navbar from "./Navbar";
 import Search from "./Search";
-import {Link} from "react-router-dom"
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import Chats from "./Chats";
@@ -19,7 +18,6 @@ const Home = () => {
             <Navbar />
             <Sidebar />
             <Chat />
-            <Link to="/chatapp/profile" className="text-dcBlue">Edit Profile (For testing purpose)</Link>
         </div>
 
     )
